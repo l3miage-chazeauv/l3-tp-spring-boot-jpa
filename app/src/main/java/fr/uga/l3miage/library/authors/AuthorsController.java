db9a360821fd9cc22b296cee3af3d864c66713be
l3miage-chazeauv <vincent.chazeau05@gmail.com>
2023-03-05 21:41:36 +0100

    21 tests passed

--- app/src/main/java/fr/uga/l3miage/library/authors/AuthorsController.java
+++ app/src/main/java/fr/uga/l3miage/library/authors/AuthorsController.java
@@ -4,11 +4,22 @@ import fr.uga.l3miage.data.domain.Author;
 import fr.uga.l3miage.library.books.BookDTO;
 import fr.uga.l3miage.library.books.BooksMapper;
 import fr.uga.l3miage.library.service.AuthorService;
+import fr.uga.l3miage.library.service.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 import java.util.Collections;
@@ -41,21 +52,63 @@ public class AuthorsController {
                 .toList();
     }
 
-    public AuthorDTO author(Long id) {
-        return null;
+    @GetMapping("/authors/{id}")
+    public AuthorDTO author(@PathVariable("id") Long id) throws EntityNotFoundException {
+        try{
+            Author aut = this.authorService.get(id);
+
+            return authorMapper.entityToDTO(aut);
+        } catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
-    public AuthorDTO newAuthor(AuthorDTO author) {
-        return null;
+    @PostMapping("/authors")
+    @ResponseStatus(HttpStatus.CREATED)
+    public AuthorDTO newAuthor(@RequestBody AuthorDTO author) {
+
+        if(author.fullName().replaceAll("\\s", "").equals("")){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        Author aut = authorMapper.dtoToEntity(author);
+        Author athS = authorService.save(aut);
+
+        return authorMapper.entityToDTO(athS);
     }
 
-    public AuthorDTO updateAuthor(AuthorDTO author, Long id) {
-        // attention AuthorDTO.id() doit être égale à id, sinon la requête utilisateur est mauvaise
-        return null;
+    @PutMapping("/authors/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public AuthorDTO updateAuthor(@RequestBody AuthorDTO author, @PathVariable("id") Long id){
+
+        if(id == author.id()){
+            try{
+                var aut = this.authorService.get(id);
+                aut.setFullName(author.fullName());
+
+                return this.authorMapper.entityToDTO(aut);
+            } catch(Exception e){
+                throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+            }
+        } else{
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 
-    public void deleteAuthor(Long id) {
-        // unimplemented... yet!
+    @DeleteMapping("/authors/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteAuthor(@PathVariable("id") Long id) {
+        try{
+            this.authorService.get(id);
+        } catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        try{
+            this.authorService.delete(id);
+        } catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+        }
     }
 
     public Collection<BookDTO> books(Long authorId) {
--- app/src/main/java/fr/uga/l3miage/library/books/BooksController.java
+++ app/src/main/java/fr/uga/l3miage/library/books/BooksController.java
@@ -1,39 +1,88 @@
 package fr.uga.l3miage.library.books;
 
+import fr.uga.l3miage.data.domain.Book;
 import fr.uga.l3miage.library.authors.AuthorDTO;
+import fr.uga.l3miage.library.service.AuthorService;
 import fr.uga.l3miage.library.service.BookService;
+import fr.uga.l3miage.library.service.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 
 @RestController
-@RequestMapping(value = "/api", produces = "application/json")
+@RequestMapping(value = "/api/v1", produces = "application/json")
 public class BooksController {
 
     private final BookService bookService;
     private final BooksMapper booksMapper;
+    private final AuthorService authorService;
 
     @Autowired
-    public BooksController(BookService bookService, BooksMapper booksMapper) {
+    public BooksController(BookService bookService, BooksMapper booksMapper, AuthorService authorService){
        this.bookService = bookService;
         this.booksMapper = booksMapper;
+        this.authorService = authorService;
     }
 
-    @GetMapping("/books/v1")
+    @GetMapping("/books")
     public Collection<BookDTO> books(@RequestParam("q") String query) {
-        return null;
+        Collection<Book> books;
+        if (query == null) {
+            books = bookService.list();
+        } else {
+            books = bookService.findByTitle(query);
+        }
+        return books.stream()
+                .map(booksMapper::entityToDTO)
+                .toList();
     }
 
+    @GetMapping("/books/{id}")
     public BookDTO book(Long id) {
-        return null;
+        try{
+            Book book = this.bookService.get(id);
+
+            return booksMapper.entityToDTO(book);
+        } catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 
-    public BookDTO newBook(Long authorId, BookDTO book) {
-        return null;
+    @PostMapping("/authors/{authorId}/books")
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookDTO newBook(@PathVariable("authorId") Long authorId, @RequestBody BookDTO book){
+
+        try{
+            authorService.get(authorId);
+        } catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        try{
+            if(book.title().replaceAll("\\s", "").equals("") || Long.toString(book.isbn()).length() < 10 || Long.toString(book.year()).length() > 4){
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+
+            var bk = this.booksMapper.dtoToEntity(book);
+            this.bookService.save(authorId, bk);
+
+            return booksMapper.entityToDTO(bk);
+        } catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+        
     }
 
     public BookDTO updateBook(Long authorId, BookDTO book) {
